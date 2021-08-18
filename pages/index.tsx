6869477c62460ac0f6dd26dc2cfaed4d512e5571
Mash Codee <codeemash@gmail.com>
2021-08-18 17:36:27 +0530

    testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,12 @@ import Head from "next/head";
 import styles from "../styles/index.module.scss";
 
 const Home: NextPage = () => {
+  const getAllLink = async () => {
+    const result = await fetch("https://localhost:4000/api/v1/link/getall");
+    const data = await result.json();
+    console.log(data);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,6 +21,7 @@ const Home: NextPage = () => {
       </Head>
 
       <h1>Hello World</h1>
+      <button onClick={() => getAllLink()}>request</button>
     </div>
   );
 };
